432273e9680d52a4a62c4b83924ec3ddeb474697
vineet416 <vineetpatel468@gmail.com>
2025-06-29 14:34:55 +0530

    setup constant

---
+++ src/constant/__init__.py
@@ -0,0 +1,15 @@
+import os
+
+
+MONGO_DATABASE_NAME = "credit_database"
+MONGO_COLLECTION_NAME = "credit_data"
+
+
+TARGET_COLUMN = "default_payment_next_month"
+MONGO_DB_URL = ""
+
+
+MODEL_FILE_NAME = "model"
+MODEL_FILE_EXTENSION = ".pkl"
+
+artifact_folder = "artifacts"
